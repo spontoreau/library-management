cad8ae3663dcd0e8d476cf2697bd69fc4e691fe9
spontoreau <pontoreau.sylvain@gmail.com>
2019-07-16 11:05:00 +0200

    Add validator

--- book/src/main.ts
+++ book/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./appModule";
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   if (process.env.NODE_ENV !== 'production') {
@@ -9,6 +10,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
 bootstrap();
